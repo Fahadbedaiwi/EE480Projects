c74109e5b06753331246eb94d30da89c27ab694c
Your Name <you@example.com>
2021-01-11 21:15:05 -0600

    Added the folder

---
+++ project1/Hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+   printf("Hello World ! my Name is fahad Bedaiwi\n");
+}
+
---
+++ project1/Hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World! my name is fahad Bedaiwi\n" ;
+}
+
+
